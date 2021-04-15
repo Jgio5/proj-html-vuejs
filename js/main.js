16326a2292f97b75ce741bf0b3767f211db5f3a7
Jorge Fabrizio Lopez Quiroz <giorgiofabrilopez@gmail.com>
2021-04-15 20:07:55 +0200

    Esercizio con Vue
    - snellire il codice

--- js/main.js
+++ js/main.js
@@ -1,5 +1,51 @@
 Vue.config.devtools = true
 
 var app = new Vue({
-    el: "#app"
+    el: "#app",
+    data: {
+        navs: [
+            'Home',
+            'Pages',
+            'Courses',
+            'Features',
+            'Blog',
+            'Shop'
+        ],
+        footerExplores: [
+            'Start here',
+            'Success story',
+            'Blog',
+            'Courses',
+            'About us',
+            'Contact us'
+        ],
+        footerInformations: [
+            'Membership',
+            'Purchase guide',
+            'Privacy policy',
+            'Terms of service'
+        ],
+        offices: [
+            {
+                street: '382 NE 191st St # 87394 Miami, Fl 33179-3899',
+                hours: '+1(305)547-9909(9am - 5pm EST,Monday - Friday)',
+                email: 'support@maxcoach.com'
+            }
+        ],
+        headerIcons: [
+            'fab fa-twitter',
+            'fab fa-facebook-f',
+            'fab fa-instagram',
+            'fab fa-linkedin'
+        ],
+        footerIcons: [
+            'fab fa-facebook-square',
+            'fab fa-facebook-f',
+            'fab fa-instagram',
+            'fab fa-linkedin'
+        ]
+    },
+
+    methods: {
+    }
 })
